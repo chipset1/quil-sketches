d7a1c26ec922f512f49540a9f2d5ee6b63bfc68f
chipset1 <stephenaemblem@gmail.com>
2016-11-11 18:27:32 -0500

    clean up caves.clj code. Move all code to caves-scratch.

--- src/quil_sketches/caves_scratch.clj
+++ src/quil_sketches/caves_scratch.clj
@@ -1,20 +1,36 @@
-(ns quil-sketches.caves
+(ns quil-sketches.caves-scratch
   (:require [quil.core :as q]
             [quil-sketches.util.core :as u]
             [quil.middleware :as m]))
 
+
+(def cell-start-alive 0.45)
+
+(take 10 (repeatedly #(< (rand) cell-start-alive)))
+
+(take 10 (repeatedly #(if (< (rand) cell-start-alive)
+                        1
+                        0)))
+
 (defn cell
   "return a new randomly set cell"
   [chance-to-start-alive]
-  (if (< (rand 1) chance-to-start-alive)
+  (if (< (rand) cell-start-alive)
     1
     0))
+
+(take 10 (repeatedly #(cell 0.45)))
+
+(repeatedly 10
+            (fn [] (take 10 (repeatedly #(cell 0.45)))))
 
 (defn cell-map
   [width height chance-to-start-alive]
   (vec (repeatedly height
                    (fn [] (vec (repeatedly width
                                            #(cell chance-to-start-alive)))))))
+
+(cell-map 10 10 0.1)
 
 (defn neighbours
   "Determines all the neighbours of a given coordinate"
@@ -24,11 +40,52 @@
         :when (not= 0 dx dy)]
     [(+ dx x) (+ dy y)]))
 
+(neighbours [1 1])
+
+(->> (neighbours [0 1])
+     (map #(get-in [[1 0 0]
+                    [0 0 1]
+                    [1 0 0]]
+                   %)))
+
+
+(count-neighbours cell-map [0 1])
+
+(->> (neighbours [0 0])
+     (map #(get-in [[1 0 0]
+                    [0 0 1]
+                    [1 0 0]]
+                   %)))
+
+(->> (neighbours [2 2])
+     (map #(get-in [[1 0 0]
+                    [0 0 1]
+                    [1 0 0]]
+                   %)))
+
+(->> (neighbours [2 2])
+     (map #(get-in [[1 0 0]
+                    [0 0 1]
+                    [1 0 0]]
+                   %))
+     (remove nil?))
+
+(->> (neighbours [2 2])
+     (keep #(get-in [[1 0 0]
+                     [0 0 1]
+                     [1 0 0]]
+                    %)))
+
 (defn count-neighbours
   [grid coord]
   (->> (neighbours coord)
        (keep #(get-in grid %1))
        (reduce +)))
+
+(count-neighbours [[1 0 0]
+                   [0 0 1]
+                   [1 0 0]]
+                  [1 1])
 
 (defn apply-rule
   [cell-map coord death-limit birth-limit]
@@ -41,6 +98,30 @@
         (if (> nbs birth-limit)
           1
           0))))
+
+(apply-rule [[1 0 0]
+             [0 0 1]
+             [1 0 0]]
+            [1 1]
+            1
+            2)
+
+(let [cell-map [[1 1 0]
+                [0 0 1]
+                [1 0 0]]
+      death-limit 3
+      birth-limit 2]
+  (loop [new-cell-map cell-map
+         coords (u/grid 3 3 1 1)]
+    (if (empty? coords)
+      new-cell-map
+      (recur (assoc-in new-cell-map
+                       (first coords)
+                       (apply-rule cell-map
+                                   (first coords)
+                                   death-limit
+                                   birth-limit))
+             (rest coords)))))
 
 (defn sim-step
   [cell-map width height death-limit birth-limit]
@@ -59,6 +140,10 @@
                                    birth-limit))
              (rest coords)))))
 
+(generate-map 1 10 10 0.45 2 2)
+
+(cell-map 3 3 0.45)
+
 (defn generate-map
   [steps width height chance-to-start-alive death-limit birth-limit]
   (let [initial-map (cell-map width
@@ -75,7 +160,7 @@
         (recur (dec s)
                (sim-step m width height death-limit birth-limit))))))
 
-(def board (generate-map 6 50 50 0.4 3 4))
+(def board (generate-map 4 50 50 0.3 3 4))
 
 (defn draw []
   (let [cell-width 10
@@ -92,6 +177,7 @@
                                                           10)))
                                               row)))
                         board))))
+
 
 (q/defsketch quil-sketches
   :size [500 500]
